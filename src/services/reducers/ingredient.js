85f3577c6600224a4c8737611e95fd0a98b83550
PC\PC <alexscandal@mail.ru>
2025-05-26 23:05:53 +0300

    Product to ingredient store

--- src/services/reducers/ingredient.js
+++ src/services/reducers/ingredient.js
@@ -2,20 +2,7 @@
 import { SET_PRODUCT } from '@/services/actions/ingredient.js';
 
 const ingredientInitialState = {
-	product: {
-		_id: '60666c42cc7b410027a1a9b1',
-		name: 'Краторная булка N-200i',
-		type: 'bun',
-		proteins: 80,
-		fat: 24,
-		carbohydrates: 53,
-		calories: 420,
-		price: 1255,
-		image: 'https://code.s3.yandex.net/react/code/bun-02.png',
-		image_mobile: 'https://code.s3.yandex.net/react/code/bun-02-mobile.png',
-		image_large: 'https://code.s3.yandex.net/react/code/bun-02-large.png',
-		__v: 0,
-	},
+	product: null,
 };
 
 export const ingredientReducer = (state = ingredientInitialState, action) => {
